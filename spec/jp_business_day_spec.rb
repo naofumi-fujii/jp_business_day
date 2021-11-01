cf1e394f23ee08265174c73f9fefc65756b0a672
naofumi-fujii <naofumi.fujii@gmail.com>
2021-11-01 17:33:58 +0900

    update tests

--- spec/jp_business_day_spec.rb
+++ spec/jp_business_day_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe JpBusinessDay do
     expect(JpBusinessDay::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  example ".first_day_of_this_month" do
+    date = Date.parse("1993-02-24")
+    expect(JpBusinessDay.first_day_of_this_month(date)).to eq Date.parse("1993-02-01")
+  end
+
+  example ".last_day_of_this_month" do
+    date = Date.parse("1993-02-24")
+    expect(JpBusinessDay.last_day_of_this_month(date)).to eq Date.parse("1993-02-28")
+  end
+
+  example ".last_business_day_of_this_month" do
+    date = Date.parse("2021-10-21")
+    expect(JpBusinessDay.last_business_day_of_this_month?(date)).to eq false
+
+    date = Date.parse("2021-10-29")
+    expect(JpBusinessDay.last_business_day_of_this_month?(date)).to eq true
   end
 end
