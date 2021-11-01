01d0d687dc8e0f886fd8682269b0d0a4366d4fa6
naofumi-fujii <naofumi.fujii@gmail.com>
2021-11-01 17:35:20 +0900

    organize methods

--- lib/jp_business_day.rb
+++ lib/jp_business_day.rb
@@ -10,12 +10,6 @@ module JpBusinessDay
   # Your code goes here...
   #
   class << self
-    def business_days(current = Time.now)
-      return (first_day_of_this_month(current)..last_day_of_this_month(current)).to_a.
-               reject { |date| date.sunday? || date.saturday? }.
-               group_by { |date| holiday?(date) }[false]
-    end
-
     def first_day_of_this_month(now = Time.now)
       Date.civil(now.year, now.month, 1)
     end
@@ -24,16 +18,18 @@ module JpBusinessDay
       Date.civil(now.year, now.month, -1)
     end
 
-    def this_month_business_days(date = Date.today)
-      business_days(date)
-    end
-
     # returns true/false
     def last_business_day_of_this_month?(date = Date.today)
       business_days(date).last == date
     end
 
     private
+
+    def business_days(current = Time.now)
+      return (first_day_of_this_month(current)..last_day_of_this_month(current)).to_a.
+               reject { |date| date.sunday? || date.saturday? }.
+               group_by { |date| holiday?(date) }[false]
+    end
 
     def holiday?(date)
       HolidayJapan.check(date)
