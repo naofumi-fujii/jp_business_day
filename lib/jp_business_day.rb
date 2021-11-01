638ee8bec9c966d370ad18613b06e59da652d49c
naofumi-fujii <naofumi.fujii@gmail.com>
2021-11-01 17:18:22 +0900

    Revert "as class Base"
    This reverts commit 77b66c3147ffa9c34ae99d536fb3978ab6407581.

--- lib/jp_business_day.rb
+++ lib/jp_business_day.rb
@@ -7,36 +7,34 @@ module JpBusinessDay
 
   # Your code goes here...
   #
-  class Base
-    class << self
-      def business_days(current = Time.now)
-        return (first_day_of_this_month(current)..last_day_of_this_month(current)).to_a.
-                 reject { |date| date.sunday? || date.saturday? }.
-                 group_by { |date| holiday?(date) }[false]
-      end
+  class << self
+    def business_days(current = Time.now)
+      return (first_day_of_this_month(current)..last_day_of_this_month(current)).to_a.
+               reject { |date| date.sunday? || date.saturday? }.
+               group_by { |date| holiday?(date) }[false]
+    end
 
-      def first_day_of_this_month(now = Time.now)
-        Date.civil(now.year, now.month, 1)
-      end
+    def first_day_of_this_month(now = Time.now)
+      Date.civil(now.year, now.month, 1)
+    end
 
-      def last_day_of_this_month(now = Time.now)
-        Date.civil(now.year, now.month, -1)
-      end
+    def last_day_of_this_month(now = Time.now)
+      Date.civil(now.year, now.month, -1)
+    end
 
-      def this_month_business_days(date = Date.today)
-        business_days(date)
-      end
+    def this_month_business_days(date = Date.today)
+      business_days(date)
+    end
 
-      # returns true/false
-      def last_business_day_of_this_month?(date = Date.today)
-        business_days(date).last == date
-      end
+    # returns true/false
+    def last_business_day_of_this_month?(date = Date.today)
+      business_days(date).last == date
+    end
 
-      private
+    private
 
-      def holiday?(date)
-        HolidayJapan.check(date)
-      end
+    def holiday?(date)
+      HolidayJapan.check(date)
     end
   end
 end
